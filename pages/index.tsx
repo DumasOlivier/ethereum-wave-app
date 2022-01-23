b049f515b2af49340612d8236eeeff4692007661
DumasOlivier <dumas.olivier@outlook.fr>
2022-01-23 19:30:20 +0100

    connect Metamask

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,90 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [currentAccount, setCurrentAccount] = useState('');
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have metamask!');
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum);
+      }
+
+      /*
+       * Check if we're authorized to access the user's wallet
+       */
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log('Found an authorized account:', account);
+        setCurrentAccount(account);
+      } else {
+        console.log('No authorized account found');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const wave = () => {
     console.log('waved');
   };
 
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Ethereum wave app</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <div className="flex w-2/3 justify-end mb-10">
+          {!currentAccount && (
+            <button
+              className="mt-5 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
+              onClick={connectWallet}
+            >
+              Connect Wallet
+            </button>
+          )}
+          {currentAccount && (
+            <div>
+              <p>You are connected 🎉</p>
+              <p>{currentAccount}</p>
+            </div>
+          )}
+        </div>
+
         <h1 className="text-6xl font-bold">Welcome</h1>
 
         <div>👋 Hey there!</div>
